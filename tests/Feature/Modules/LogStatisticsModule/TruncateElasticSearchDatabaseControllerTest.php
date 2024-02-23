3faec2c3325501992c52c53230f826f81d63e3ae
unknown <muhammad1370@gmail.com>
2024-02-23 19:21:14 +0200

    TDD add TruncateElasticSearchDatabaseControllerTest to check delete controller

---
+++ tests/Feature/Modules/LogStatisticsModule/TruncateElasticSearchDatabaseControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class TruncateElasticSearchDatabaseControllerTest extends WebTestCase
+{
+    public function testDeleteElasticSearchIndices()
+    {
+        // Arrange
+        $client = static::createClient();
+
+        // Act
+        $client->request('DELETE', '/delete');
+
+        // Assert
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $client->getResponse()->getStatusCode());
+    }
+}
