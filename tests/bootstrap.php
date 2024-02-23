0a3ce8c316d010ca270a68fbbac9569ac6d22975
unknown <muhammad1370@gmail.com>
2024-02-24 01:19:36 +0200

    Fix migrations before running tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,5 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
+system('php bin/console doctrine:migration:migrate prev --no-interaction');
+system('php bin/console doctrine:migration:migrate --no-interaction');
