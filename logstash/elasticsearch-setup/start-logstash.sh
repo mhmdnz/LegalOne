23b3117ef211929165a02419048169995ba5bb5c
unknown <muhammad1370@gmail.com>
2024-02-24 02:03:35 +0200

    Trying to fix start-logstash.sh permission issue

--- logstash/elasticsearch-setup/start-logstash.sh
+++ logstash/elasticsearch-setup/start-logstash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Running Elasticsearch setup script..."
+chmod +x /elasticsearch-setup/setup-elasticsearch.sh
 /elasticsearch-setup/setup-elasticsearch.sh
 
 echo "Starting Logstash..."
